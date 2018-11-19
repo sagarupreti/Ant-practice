dd4cfcc2f6f265e2bc52b0499cef6ea865dd7ec9
sagarupreti <uprsagar@gmail.com>
2018-11-19 08:06:08 +0545

    first commit

---
+++ src/com/cibt/app/program.java
@@ -0,0 +1,10 @@
+package com.cibt.app;
+
+public class program{
+    public static void main(String[] args){
+        for(int i=1;i<=10;i++){
+            System.out.println("Hello World");
+        }
+
+    }
+}
